478da0b7a1e95e1a54c53ac867a7de1d33684fd4
luhuayue <luhuayueapp@163.com>
2020-01-17 14:47:59 -0800

    folder with save and download sript in it

---
+++ src/save_download.R
@@ -0,0 +1,22 @@
+# author: Huayue Lu
+# date: 2020-01-17
+#
+# This script download and save the data that are in CSV 
+# format, from https://www.kaggle.com/karangadiya/fifa19
+#
+# Usage: download_save.R
+
+library(tidyverse)
+library(testthat)
+
+main <- function(){
+  
+  # read in data
+  data_fifa <- read.csv("https://raw.githubusercontent.com/mglu123/live_dash_demo/master/data-2.csv")
+  
+  # save the  data
+  write.csv(data_fifa,'data_fifa.csv')
+}
+
+
+main()
