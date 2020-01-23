feddc640038c075000bdce1dec4c26e25f64d47a
luhuayue <luhuayueapp@163.com>
2020-01-22 21:02:09 -0800

    improve script which can save two splited csv files

--- src/save_download.R
+++ src/save_download.R
@@ -5,8 +5,9 @@
 format, from https://www.kaggle.com/karangadiya/fifa19.
 This data was kept here :
 https://raw.githubusercontent.com/mglu123/live_dash_demo/master/data-2.csv
-Save data as name data_fifa.csv in /data/raw folder
-Usage: download_save.R <file_path> <url>
+Save the splited data as name data_fifa_train.csv for training dataset
+and data_fifa_test.csv for test dataset in specified absolute path.
+Usage: download_save.R <file_path_train> <file_path_test> <url>
 " -> doc
 
 library(tidyverse)
@@ -17,12 +18,15 @@ library(RCurl)
 opt <- docopt(doc)
 
 
-main <- function(file_path,url){
+main <- function(file_path_train, file_path_test, url){
   # read in data
   data_fifa <- read.csv(url)
-  
+  num_rows <- dim(data_fifa)[1] - dim(data_fifa)[[1]]%/%5
+  data_fifa_train <- data_fifa[1:num_rows,]
+  data_fifa_test <- data_fifa[num_rows:dim(data_fifa)[1],]
   # save the  data
-  write_csv(data_fifa, file_path)
+  write.csv(data_fifa_train, file_path_train, row.names = TRUE)
+  write.csv(data_fifa_test, file_path_test, row.names = TRUE)
 }
 
 test_main <- function(url) {
@@ -32,4 +36,4 @@ test_main <- function(url) {
 }
 test_main(opt$url)
 
-main(opt$file_path, opt$url)
+main(opt$file_path_train, opt$file_path_test, opt$url)
