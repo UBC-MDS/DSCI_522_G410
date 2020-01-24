6deb40cb7e4b2d8df90dc9cddb51d52bb6728016
Holly Williams <hwilliams@srk.com>
2020-01-24 12:47:17 -0800

    script to download and save league and club info

---
+++ src/download_and_save_league_data.R
@@ -0,0 +1,44 @@
+# author: Huayue Lu G410, Holly Williams
+# date: 2020-01-24
+
+
+
+"This script is for downloading and saving data only.
+This was used to download league data from https://github.com/hwilliams10/fifa_data
+which was created by myself manually using this website https://www.ea.com/games/fifa/news/fifa-19-leagues-and-teams
+Writes the data to a csv file 
+
+Usage: src/save_download_only.r --url=<url> --out_dir=<out_dir>
+
+Options:
+--url=<url>           URL link to download data from
+--out_dir=<out_dir>   Path to directory where the file should be written
+" -> doc
+
+
+library(tidyverse)
+library(testthat)
+library(docopt)
+library(RCurl)
+
+opt <- docopt(doc)
+
+main <- function(url, out_dir){
+
+  # read in data
+  data <- read.csv(url)
+  # save to file
+  write.csv(data, paste0(out_dir, "clubs_and_leagues.csv"), row.names = TRUE)
+}
+
+
+
+test_main <- function(url) {
+  test_that("test that url exists", {
+    expect_true(url.exists(url))
+  })
+}
+
+test_main(opt$url)
+
+main(opt$url, opt$out_dir)
